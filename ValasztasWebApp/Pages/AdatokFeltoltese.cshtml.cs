56f2eb36c1eff3049f490d125422520420edaf17
Gyarmati Csanád <gyarmati.csanad@burg.lo.hu>
2024-09-25 11:47:58 +0200

    masodik

--- ValasztasWebApp/Pages/AdatokFeltoltese.cshtml.cs
+++ ValasztasWebApp/Pages/AdatokFeltoltese.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ValasztasWebApp.Models;
 
 namespace ValasztasWebApp.Pages
 {
@@ -8,8 +9,14 @@ namespace ValasztasWebApp.Pages
 
 
         private readonly IWebHostEnvironment _env;
+        private readonly ValasztasDbContext _context;
 
-
+        public AdatokFeltolteseModel
+            (IWebHostEnvironment env,
+            ValasztasDbContext context)
+        { _context = context;
+            _env = env;
+        }
 
 
 
@@ -26,6 +33,28 @@ namespace ValasztasWebApp.Pages
         
         public void OnGet()
         {
+        }
+
+
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var UploadFilePath = Path.Combine(_env.ContentRootPath
+                , "Uploads", UploadFile.FileName);
+
+            using (var stream = new FileStream(UploadFilePath,FileMode.Create
+                ))
+            {
+                await UploadFile.CopyToAsync(stream);
+            }
+
+            StreamReader sr = new StreamReader(UploadFilePath);
+            sr.Close();
+
+
+
+                return Page();
         }
     }
 }
